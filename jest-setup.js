e149ea2e7b01b9f256031b5a3b119c337b058b60
Fernando Larrañaga <xabadu@gmail.com>
2017-06-30 14:49:49 -0700

    Add firebase, console, Geolocation, ScrollView and Linking (#2)

---
+++ jest-setup.js
@@ -0,0 +1,43 @@
+jest.mock('Linking', () => ({
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  openURL: jest.genMockFn().mockReturnValue(Promise.resolve()),
+  canOpenURL: jest.genMockFn().mockReturnValue(Promise.resolve()),
+  getInitialURL: jest.genMockFn().mockReturnValue(Promise.resolve()),
+}));
+
+/* Geolocation, required by the navigation global */
+jest.mock('Geolocation', () => ({
+  clearWatch: jest.fn(),
+  getCurrentPosition: jest.genMockFn().mockReturnValue({
+    city: 'San Francisco',
+    state: 'CA',
+    lat: 37.785834,
+    lng: -122.406417
+  }),
+  stopObserving: jest.fn(),
+  watchPosition: jest.fn()
+}));
+
+jest.mock('ScrollView', () => jest.genMockFromModule('ScrollView'));
+
+/* Firebase */
+jest.mock('firebase', () => ({
+  initializeApp: jest.fn(),
+  addEventListener: jest.fn(),
+  attachEvent: jest.fn(),
+  auth: jest.genMockFn().mockReturnValue({
+    onAuthStateChanged: jest.fn(),
+    signOut: jest.genMockFn().mockReturnValue({})
+  }),
+  database: jest.genMockFn().mockReturnValue({ 
+    ref: jest.genMockFn().mockReturnValue({ 
+      once: jest.genMockFn().mockReturnValue({}),
+      orderByKey: jest.genMockFn().mockReturnValue({
+        once: jest.genMockFn().mockReturnValue({})
+      })
+    })
+  })
+}));
+
+console.error = jest.genMockFn();
