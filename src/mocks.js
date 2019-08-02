48b4784a3d68e3a7fe57ab2bea2bbedcc155bea1
Karen Serfaty <keserfaty@gmail.com>
2019-08-02 11:39:42 +0800

    add rn localize

--- src/mocks.js
+++ src/mocks.js
@@ -32,6 +32,46 @@ module.exports = {
     jest.mock('ScrollView', () => jest.genMockFromModule('ScrollView'));
     jest.mock('YellowBox', () => jest.genMockFromModule('YellowBox'));
 
+    //* react-native-localize
+    const getLocales = () => [
+      { countryCode: 'US', languageTag: 'en-US', languageCode: 'en', isRTL: false },
+      { countryCode: 'FR', languageTag: 'fr-FR', languageCode: 'fr', isRTL: false }
+    ];
+
+    const findBestAvailableLanguage = () => ({
+      languageTag: 'en-US',
+      isRTL: false
+    });
+
+    const getNumberFormatSettings = () => ({
+      decimalSeparator: '.',
+      groupingSeparator: ','
+    });
+
+    const getCalendar = () => 'gregorian';
+    const getCountry = () => 'US';
+    const getCurrencies = () => ['USD', 'EUR'];
+    const getTemperatureUnit = () => 'celsius';
+    const getTimeZone = () => 'Europe/Paris';
+    const uses24HourClock = () => true;
+    const usesMetricSystem = () => true;
+
+    const addEventListener = jest.fn();
+    const removeEventListener = jest.fn();
+
+    jest.mock('findBestAvailableLanguage', findBestAvailableLanguage);
+    jest.mock('getLocales', getLocales);
+    jest.mock('getNumberFormatSettings', getNumberFormatSettings);
+    jest.mock('getCalendar', getCalendar);
+    jest.mock('getCountry', getCountry);
+    jest.mock('getCurrencies', getCurrencies);
+    jest.mock('getTemperatureUnit', getTemperatureUnit);
+    jest.mock('getTimeZone', getTimeZone);
+    jest.mock('uses24HourClock', uses24HourClock);
+    jest.mock('usesMetricSystem', usesMetricSystem);
+    jest.mock('addEventListener', addEventListener);
+    jest.mock('removeEventListener', removeEventListener);
+
     console.error = jest.fn();
   }
 };
